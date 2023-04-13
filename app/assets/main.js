7bd63998124b0484d5b47e2a78308712bb6134d7
Thomas Verschoren <thomas@verschoren.com>
2023-04-13 20:52:41 +0200

    Add balls

--- app/assets/main.js
+++ app/assets/main.js
@@ -2,7 +2,7 @@
     // Initialise Apps framework client. See also:
     // https://developer.zendesk.com/apps/docs/developer-guide/getting_started
     var client = ZAFClient.init();
-    client.invoke('resize', { width: '100%', height: '200px' });
+    client.invoke('resize', { width: '100%', height: '600px' });
 
     init();
     async function init() {
@@ -14,6 +14,26 @@
         console.log(linked_pokemon);
         var pokemon_data = await getPokemon(linked_pokemon);
         console.log(pokemon_data);
+        /*
+            hp: 42
+            pokeball: "01GXXWVG2SG5QNFE6KEG9S0S5B"
+            pokemon_species: "01GXXWN869WA5X6SSQKHSC7RJD"
+            shiny: false
+            trainer: "10992004688146"
+        */
+        var pokeball = await getBall(pokemon_data.custom_object_record.custom_object_fields.pokeball);
+        console.log(pokeball);
+        var pokemon_species = await getSpecies(pokemon_data.custom_object_record.custom_object_fields.pokemon_species);
+        console.log(pokemon_species);
+        /*
+        evolves_into: "01GXXWMRCD1GAH0HNAY36DWJ7X"
+        pokedex_number: 1
+        type: "01GXXW5DKCD4A5T0559MSP2AS0"
+        */
+       var type = getTypes(pokemon_species.custom_object_record.custom_object_fields.type);
+        console.log(type)
+        var evolves = await getSpecies(pokemon_species.custom_object_record.custom_object_fields.evolves_into);
+        console.log(evolves);
     }
     
     
@@ -24,9 +44,42 @@
             dataType: 'json'
         }).then(
             function(pokemon) {
-                console.log(pokemon);
                 return pokemon
             }
         );
-    }//use a zendesk get to retrieve data from api/v2/custom_objects/{custom_object_key}/records
+    }
 
+    async function getSpecies(species){
+        return await client.request({
+            url: '/api/v2/custom_objects/pokemon/records/'+species+'.json',
+            type: 'GET',
+            dataType: 'json'
+        }).then(
+            function(pokemon) {
+                return pokemon
+            }
+        );
+    }
+    async function getBall(ball){
+        return await client.request({
+            url: '/api/v2/custom_objects/pokeball/records/'+ball+'.json',
+            type: 'GET',
+            dataType: 'json'
+        }).then(
+            function(pokemon) {
+                return pokemon
+            }
+        );
+    }
+
+    async function getTypes(types){
+        return await client.request({
+            url: '/api/v2/custom_objects/pokemon_type/records/'+types+'.json',
+            type: 'GET',
+            dataType: 'json'
+        }).then(
+            function(pokemon) {
+                return pokemon
+            }
+        );
+    }
