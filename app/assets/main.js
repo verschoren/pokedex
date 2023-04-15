d4313bdca83efff6918481b626f7eeec0af5d422
Thomas Verschoren <thomas@verschoren.com>
2023-04-15 12:26:53 +0200

    Upload screenshots

--- app/assets/main.js
+++ app/assets/main.js
@@ -11,10 +11,9 @@
             //return ticket.customField:custom_field_10991970231570
             return custom_field['ticket.customField:custom_field_10991970231570'];
         })
-        console.log(linked_pokemon);
         var pokemon_data = await getPokemon(linked_pokemon);
-        console.log(pokemon_data);
-        /*
+       
+       console.log(pokemon_data) /*
             hp: 42
             pokeball: "01GXXWVG2SG5QNFE6KEG9S0S5B"
             pokemon_species: "01GXXWN869WA5X6SSQKHSC7RJD"
@@ -22,21 +21,31 @@
             trainer: "10992004688146"
         */
         var pokeball = await getBall(pokemon_data.custom_object_record.custom_object_fields.pokeball);
-        console.log(pokeball);
         var pokemon_species = await getSpecies(pokemon_data.custom_object_record.custom_object_fields.pokemon_species);
-        console.log(pokemon_species);
         /*
         evolves_into: "01GXXWMRCD1GAH0HNAY36DWJ7X"
         pokedex_number: 1
         type: "01GXXW5DKCD4A5T0559MSP2AS0"
         */
-       var type = getTypes(pokemon_species.custom_object_record.custom_object_fields.type);
-        console.log(type)
+        var type = await getTypes(pokemon_species.custom_object_record.custom_object_fields.type);
         var evolves = await getSpecies(pokemon_species.custom_object_record.custom_object_fields.evolves_into);
-        console.log(evolves);
+        updateUI(pokemon_data,pokeball,pokemon_species,type,evolves);
     }
+
     
-    
+    function updateUI(pokemon_data,pokeball,pokemon_species,type,evolves){
+        $('#name').html(pokemon_data.custom_object_record.name)
+        $('#hp').html(pokemon_data.custom_object_record.hp)
+        $('#shiny').html(pokemon_data.custom_object_record.shiny)
+        $('#image').attr('src','https://pokedex.verschoren.dev/images/'+pokemon_species.custom_object_record.name.toLowerCase()+'.png')
+        $('#index').html('Pokedex #'+pokemon_species.custom_object_record.custom_object_fields.pokedex_number)
+        $('#type').html(type.custom_object_record.name)
+        $('#evolves').html(evolves.custom_object_record.name)
+        $('#evolves_image').attr('src','https://pokedex.verschoren.dev/images/'+evolves.custom_object_record.name.replaceAll(' ','').toLowerCase()+'.png')
+        $('#ball').html(pokeball.custom_object_record.name)
+        $('#ball_image').attr('src','https://pokedex.verschoren.dev/images/'+pokeball.custom_object_record.name.replaceAll(' ','').toLowerCase()+'.png')
+    }
+
     async function getPokemon(linked_pokemon){
         return await client.request({
             url: '/api/v2/custom_objects/pokemon_captured/records/'+linked_pokemon+'.json',
