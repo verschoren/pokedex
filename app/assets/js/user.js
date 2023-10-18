c91136bd2d527af211f370f25bc6e59643fdf752
Thomas Verschoren <thomas@verschoren.com>
2023-10-18 13:16:30 +0200

    Add badges

---
+++ app/assets/js/user.js
@@ -0,0 +1,10 @@
+var client = ZAFClient.init();
+client.invoke('resize', { width: '100%', height: '600px' });
+
+$(document).ready(async function() {
+    init();
+});
+
+async function init() {
+
+}
